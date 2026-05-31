9a1984d2cf79c890e7ef88acdffcea25bc1e69b1
aditya-pm <adityapmenon29@gmail.com>
2026-05-31 14:02:55 +0530

    Update Main Menu

--- Views/MainMenuView.cs
+++ Views/MainMenuView.cs
@@ -8,9 +8,51 @@ static class MainMenuView
     {
         AnsiConsole.Clear();
 
-        Panel panel = new Panel("[bold green]DEVTRACK CLI[/]");
+        var titleText = new FigletText("DevTrack CLI")
+        {
+            Color = Color.DarkSeaGreen,
+            Justification = Justify.Center
+        };
+
+        var rows = new Rows(
+            titleText,
+            Align.Center(new Markup("[green]A Developer Productivity Tool[/]"))
+        );
+
+        Panel panel = new Panel(rows);
         panel.Border = BoxBorder.Double;
+
+        var rootGrid = new Grid();
+        rootGrid.AddColumns(2);
+
+        var githubGrid = new Grid();
+        githubGrid.AddColumn();
+        githubGrid.AddRow("GitHub Analytics");
+        githubGrid.AddRow(new Rule());
+        githubGrid.AddRow("• Repo Name");
+        githubGrid.AddRow("• Open Issues");
+        githubGrid.AddRow("• Recent Commits");
+        githubGrid.AddRow("• Languages");
+        githubGrid.AddRow("• Fork count");
+
+        var githubPanel = new Panel(githubGrid).NoBorder();
+
+        var taskGrid = new Grid();
+        taskGrid.AddColumn();
+        taskGrid.AddRow("Task Manager");
+        taskGrid.AddRow(new Rule());
+        taskGrid.AddRow("• Add task");
+        taskGrid.AddRow("• Complete task");
+        taskGrid.AddRow("• Delete task");
+
+        var taskPanel = new Panel(taskGrid).NoBorder();
+
+        rootGrid.AddEmptyRow();
+        rootGrid.AddRow(githubPanel, taskPanel);
+        rootGrid.AddEmptyRow();
+
         AnsiConsole.Write(panel);
+        AnsiConsole.Write(rootGrid);
 
         var choice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
